a3e027580efed573cdea3cf1aa8cce86f31d74e5
Wahyu Arzani <hyuarzani@gmail.com>
2021-04-16 18:44:05 +0700

    add navigation hook

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 import {Navigation} from 'react-native-navigation';
+import {withNavigationProvider} from 'react-native-navigation-hooks';
 import App from './App';
 
-Navigation.registerComponent('com.myApp.WelcomeScreen', () => App);
+Navigation.registerComponent('com.myApp.WelcomeScreen', () => {
+  return withNavigationProvider(App);
+});
 Navigation.events().registerAppLaunchedListener(() => {
   Navigation.setRoot({
     root: {
